0ca276ca7bc151590dfd1ade7ad52d05b0a69392
ritika dubey <ritikadubey1305@gmail.com>
2026-08-31 21:00:53 +0530

    new file

---
+++ first.py
@@ -0,0 +1,13 @@
+import pyjokes
+
+# print("printing jokes") (ctrl+/)
+# this prints a random joke (single line comment)
+
+""" 
+so thanks 
+that was 
+my first program
+"""
+joke=pyjokes.get_joke()
+print(joke)
+
