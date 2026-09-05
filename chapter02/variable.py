321bf200a60ca3450b49efd277a8cbffeb5b9cd0
ritika dubey <ritikadubey1305@gmail.com>
2026-09-05 14:26:17 +0530

    commit

--- chapter02/variable.py
+++ chapter02/variable.py
@@ -5,3 +5,18 @@ c="ritika" #keywords
 b=8.88 #identifiers
 
 print(a+b)
+
+# truth table of or 
+e=True or False
+print("true or false is ",True or False)
+print("true or true is ",True or False)
+print("false or true is ",True or False)
+print("false or false is ",False or False )
+print()
+
+print("true and false is ",True and False)
+print("true and true is ",True and True)
+print("false and true is ",True and False)
+print("false and false is ",False and False)
+
+print(not(True))
